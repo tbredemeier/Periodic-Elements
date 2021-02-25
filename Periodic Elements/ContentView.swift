9cbf5f0ecce120e3798ff10060a21080ea0143b0
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-24 21:14:12 -0600

    Parse remaining JSON data

--- Periodic Elements/ContentView.swift
+++ Periodic Elements/ContentView.swift
@@ -29,7 +29,10 @@ struct ContentView: View {
                 let contents = json.arrayValue
                 for item in contents {
                     let name = item["name"].stringValue
-                    let element = Element(name: name)
+                    let symbol = item["symbol"].stringValue
+                    let facts = item["facts"].stringValue
+                    let history = item["history"].stringValue
+                    let element = Element(symbol: symbol, name: name, history: history, facts: facts)
                     elements.append(element)
                 }
             }
@@ -45,5 +48,8 @@ struct ContentView_Previews: PreviewProvider {
 
 struct Element: Identifiable {
     let id = UUID()
+    var symbol: String
     var name: String
+    var history: String
+    var facts: String
 }
