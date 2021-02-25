bcd16183b133b4b399541226cb1249e836dc1bc1
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-24 20:55:11 -0600

    Initial Commit

---
+++ Periodic Elements/Periodic_ElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Periodic_ElementsApp.swift
+//  Periodic Elements
+//
+//  Created by Tom Bredemeier on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Periodic_ElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
